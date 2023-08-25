56547579249e58422817aba07fd85fe133fdfe5d
Jensone <hello@jensone.com>
2023-08-25 16:33:56 +0200

    🔧 fix(index.js): fix indentation and add comments to improve code readability 🔧 fix(package.json): add express-openid-connect as a dependency ✨ feat(views/index.ejs): update content to display a welcome message and links to profile and logout ✨ feat(views/login.ejs): create a new file to display a message and a link to login ✨ feat(views/profile.ejs): create a new file to display user profile information and a link to logout

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
-const express = require('express');
+// Importer express
+const express = require("express");
 
+// Initialiser express
 const app = express();
 
-app.set('view engine', 'ejs');
+// Configurer le moteur de template EJS
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-    res.render('index');
+// ---------- Code de Auth0 -------- //
+
+// 1. Importer express-openid-connect
+const { auth } = require("express-openid-connect");
+
+// 2. Configurer avec un objet les paramètres de connexion Auth0
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: "a long, randomly-generated string stored in env",
+    baseURL: "http://localhost:3000",
+    clientID: "7pMy8GrsX7I6xgVk1OsUYz4RAVMnUcZS",
+    issuerBaseURL: "https://dev-m6hbnwzejhsfrvsw.eu.auth0.com",
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get("/", (req, res) => {
+    res.render(req.oidc.isAuthenticated() ? 'index' : 'login');
 });
 
+// Profil
+const { requiresAuth } = require('express-openid-connect');
+
+app.get('/profile', requiresAuth(), (req, res) => {
+    const userData = JSON.stringify(req.oidc.user);
+    const user = JSON.parse(userData);
+    console.log(user);
+    res.render('profile', { user });
+});
+
+// ---------- Fin du code de Auth0 -------- //
+
+// Lance le serveur sur le port 3000
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+    console.log("Server is running on port 3000");
 });
